2f2de7ae56e14628aa8ce2d311b8647a9436fde5
DevinFauzan <devinfrr@gmail.com>
2019-11-21 23:15:33 +0700

    Tutor 2

---
+++ appkontrolui/app/src/main/java/com/example/appkontrolui/Activity1.java
@@ -0,0 +1,10 @@
+package com.example.appkontrolui;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Activity1 extends Activity {
+    protected void onCreate(Bundle savedInstanceState)
+    { super.onCreate(savedInstanceState); setContentView(R.layout.control_textview);
+    }
+}
